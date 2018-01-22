55189b2bc08c2cce27077b27a999fcf110a1ac65
Nikita <mineralsfree@gmail.com>
2018-01-22 17:21:50 +0300

    w
    w

--- CreateField.pas
+++ CreateField.pas
@@ -60,18 +60,19 @@ var i,j:Byte;
 // Test1:string;
   procedure Seabattle_fieldAI_generator(var P2F:TPF);
   const ships = 20;
-   var ib,jb,ie,je,i,j,ship_size,ship_amount,direction,n:Integer ;
- var exiter:Boolean;
+   var ib,jb,ie,je,i,j,k,ship_size,ship_amount,direction,n:Integer ;
+ var exiter,exc:Boolean;
  function Quick_math(x,y:integer):boolean;
  var count:integer;
-  begin
+
+ begin
   count:=0;
-   if     (P2F[x-1,y]='K') or (P2F[x,y-1]='K') or (P2F[x-1,y-1]='K')
-      and (P2F[x+1,y]='K') or (P2F[x,y+1]='K') or (P2F[x+1,y+1]='K')
-      and (P2F[x+1,y-1]='K')or (P2F[x-1,y+1]='K')  then
-   inc(count);
-   if count<=2 then
-   Quick_math:=true;
+   if
+     (P2F[x-1,y]='K') or (P2F[x,y-1]='K') or (P2F[x-1,y-1]='K')
+      or (P2F[x+1,y]='K') or (P2F[x,y+1]='K') or (P2F[x+1,y+1]='K')
+      or (P2F[x+1,y-1]='K')or (P2F[x-1,y+1]='K')  then
+   Quick_math:=true
+   else quick_math:=false;
  end;
  begin
  n:=10;
@@ -83,44 +84,78 @@ var i,j:Byte;
  exiter:=False;
  Ship_size:=4;
  ship_amount:=1;
-  repeat
+  for k := 1 to 4 do
+  begin
 
     for j := 1 to ship_amount do
     begin
     repeat
+      exiter:=False;
       repeat
         ib:=Random(n)+1;
         jb:=Random(n)+1;
-      until  ((P2F[ib,jb])<>'K')
-      and (P2F[ib-1,jb]<>'K') and (P2F[ib,jb-1]<>'K') and (P2F[ib-1,jb-1]<>'K')
-      and (P2F[ib+1,jb]<>'K') and (P2F[ib,jb+1]<>'K') and (P2F[ib+1,jb+1]<>'K')
-      and (P2F[ib+1,jb-1]<>'K')and (P2F[ib-1,jb+1]<>'K');
+      until not(quick_math(ib,jb)) or ( (jb+ship_size-1>10) or (ib+ship_size-1>10)) ;
       direction:=random(2)+1;             //1-Right 2-Bottom 3-Left 4-up
+      if ship_size>1 then
+      begin
       case direction of
       1:
-      if (P2F[ib+ship_size-1,jb]=' ') then
+      if  (ib+ship_size-1<=10) then
         begin
-        P2F[ib,jb]:='K';
+
+        exc:=True;
         for I := 1 to ship_size-1 do
         if Quick_math(ib+i,jb) then
-        P2F[ib+i,jb]:='K';
-        exiter:=true;
+        exc:=False;
+         if exc  then
+         for I := 1 to ship_size-1 do
+         begin
+         P2F[ib,jb]:='K';
+         P2F[ib+i,jb]:='K';
+         exiter:=true;
+         end;
+
+
         end;
       2:
-      if  (P2F[ib,jb+ship_size-1]=' ')then
+      if   (jb+ship_size-1<=10)then
         begin
-        P2F[ib,jb]:='K';
+         exc:=True;
         for I := 1 to ship_size-1 do
         if Quick_math(ib,jb+i) then
-        P2F[ib,jb+i]:='K';
-        exiter:=true;                                                                      //1-Right 2-Bottom 3-Left 4-up
+        exc:=False;
+         if exc  then
+
+         for I := 1 to ship_size-1 do
+         begin
+          P2F[ib,jb]:='K';
+          P2F[ib,jb+i]:='K';
+          exiter:=true;
+
+         end
+        end;                                                                      //1-Right 2-Bottom 3-Left 4-up
         end;
+        end
+      else begin
+      P2F[ib,jb]:='K';
+      exiter:=true;
       end;
     until(exiter) ;
-   end;
+    end;
     inc(ship_amount);
     dec(ship_size);
-  until (ship_amount=5);
+
+  {   for I := 1 to n do
+  begin
+    for j := 1 to n do
+      write (P2F[i,j]);
+      writeln;
+  end;    }
+
+
+
+
+  end;
 end;
 
 
