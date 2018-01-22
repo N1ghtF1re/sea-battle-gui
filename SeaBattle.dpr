64cf7b218fcd43e176be14b93b370a2f8058f620
Alexandr Pankratiew <sasha.pankratiew@gmail.com>
2018-01-22 11:19:49 +0300

    Create Main Page

--- SeaBattle.dpr
+++ SeaBattle.dpr
@@ -2,14 +2,16 @@ program SeaBattle;
 
 uses
   Vcl.Forms,
-  Unit1 in 'Unit1.pas' {Form1},
-  Unit2 in 'Unit2.pas' {Form2};
+  MainPage in 'MainPage.pas' {Form1},
+  CreateField in 'CreateField.pas' {Form1},
+  ErrorPage in 'ErrorPage.pas' {Form2};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm3, Form3);
   Application.CreateForm(TForm1, Form1);
   Application.CreateForm(TForm2, Form2);
   Application.Run;
